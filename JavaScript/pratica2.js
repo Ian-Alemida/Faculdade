9abd8bff38904faac755e1f702f20b1a230435e4
Ian Almeida <iangtaonline1@gmail.com>
2023-08-27 20:23:34 -0300

    estudando

--- JavaScript/pratica2.js
+++ JavaScript/pratica2.js
@@ -6,13 +6,13 @@ let bebida = parseInt(rlSync.question("Qual bebida voce deseja? \n"));
 
 switch (bebida){
     case 1:
-        console.log("Você escolheu chá");
+        console.log("Você escolheu chá: R$ 4,00");
         break;
     case 2:
-        console.log("Você escolheu café");
+        console.log("Você escolheu café: R$ 5,00");
         break;
     case 3:
-        console.log("Você escolheu leite");
+        console.log("Você escolheu leite: R$ 7,00");
         break;
     default:
         console.log("bebida invalida");
